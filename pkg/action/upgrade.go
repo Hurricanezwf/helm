06ab29c1aeb1ddb8d25d3053d22e5717d948f5aa
zhouwenfeng <zwf1094646850@gmail.com>
2022-09-19 23:10:07 +0800

    hotfix: 移除将当前 manifest 替换为集群中的资源状态

--- pkg/action/upgrade.go
+++ pkg/action/upgrade.go
@@ -211,24 +211,25 @@ func (u *Upgrade) prepareUpgrade(ctx context.Context, name string, chart *chart.
 		}
 	}
 
+	// Note: 这一步不能打开, Helm 的垃圾 patch 行为会让他无法正常工作!!!
 	// ZWF: 将 currentRelease 中的 manifest 替换为实时状态, 不再以 secret 中记录的 helm 元数据为准.
-	currentObjects, err := u.cfg.KubeClient.BuildToUnstructured(ctx, currentRelease.Manifest)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to build current release's manifest to objecsts, %w", err)
-	}
-	jsonChunks := make([]string, 0, len(currentObjects))
-	for _, obj := range currentObjects {
-		b, err := obj.MarshalJSON()
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed to marshal object to json, %w", err)
-		}
-		jsonChunks = append(jsonChunks, string(b))
-	}
-	currentReleaseManifest, err := encoding.JSONChunkToYAMLStream(jsonChunks)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to encode json chunks to yaml stream, %w", err)
-	}
-	currentRelease.Manifest = currentReleaseManifest
+	//currentObjects, err := u.cfg.KubeClient.BuildToUnstructured(ctx, currentRelease.Manifest)
+	//if err != nil {
+	//	return nil, nil, fmt.Errorf("failed to build current release's manifest to objecsts, %w", err)
+	//}
+	//jsonChunks := make([]string, 0, len(currentObjects))
+	//for _, obj := range currentObjects {
+	//	b, err := obj.MarshalJSON()
+	//	if err != nil {
+	//		return nil, nil, fmt.Errorf("failed to marshal object to json, %w", err)
+	//	}
+	//	jsonChunks = append(jsonChunks, string(b))
+	//}
+	//currentReleaseManifest, err := encoding.JSONChunkToYAMLStream(jsonChunks)
+	//if err != nil {
+	//	return nil, nil, fmt.Errorf("failed to encode json chunks to yaml stream, %w", err)
+	//}
+	//currentRelease.Manifest = currentReleaseManifest
 
 	// determine if values will be reused
 	vals, err = u.reuseValues(chart, currentRelease, vals)
